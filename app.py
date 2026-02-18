25e23b879b7f030ad5183840920d0ec989e86ce3
Nilesh Sarkar <sarkarnilesh334@gmail.com>
2026-02-18 20:32:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,6 @@ def search():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 8080))  # ← NEW
+    app.run(host='0.0.0.0', port=port, debug=True)
+
